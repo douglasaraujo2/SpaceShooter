a71bc8be57aae016f7b56b7cddc9197a576c7aa4
logonrm <logonrm@fiap.com.br>
2017-12-11 21:35:51 -0200

    New commit

---
+++ Assets/Scripts/JogadorScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JogadorScript : MonoBehaviour {
+
+	public float velocidade;
+	public float limiteEsquerdo, limiteDireito;
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		Mover ();
+
+			
+	}
+
+
+	void Mover(){
+		float move_x = Input.GetAxisRaw("Horizontal") * velocidade * Time.deltaTime;
+		float move_y = Input.GetAxisRaw("Vertical") * velocidade * Time.deltaTime;
+		transform.Translate (move_x, move_y, 0.0f);
+		if (transform.position.x < limiteEsquerdo || transform.position.x > limiteDireito) {
+			transform.position = new Vector2 (transform.position.x * -1 ,transform.position.y);
+		}
+	}
+}
---
+++ Assets/Scripts/ProjetilScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjetilScript : MonoBehaviour {
+
+	public float velocidade;
+	public float tempoDeVida;
+
+	// Use this for initialization
+	void Start () {
+		Destroy (gameObject, tempoDeVida);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector2.up * velocidade * Time.deltaTime);
+	}
+
+	void OnCollisionEnter2D(Collision2D c){
+		if (c.gameObject.tag == "Inimigo") {
+			Destroy (gameObject);
+		}
+
+	}
+}
